7c220c5df96db9584407d8595f4e3d82ac4fa2a4
Johnny Cowboy <johnnycowboy3033@gmail.com>
2024-04-24 18:56:32 -0400

    commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ $(function() {
     //Webpage site layout
     consoleNotebook.insertCodeBlock('code-1',0,43);
 
+    consoleNotebook.formatClear().manyNewLine(6);
+    consoleNotebook.setClass("bottom-page");
+
      //Algorithm logging
     
   });
