f4f32dbf7602b1cabd46ffff6de04a91f8d06f3f
Johnny Cowboy <johnnycowboy3033@gmail.com>
2024-04-24 18:00:32 -0400

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+    
+    let consoleNotebook = new ConsoleNotebook([['Head','Midpoint','Tail']]);
+
+    //Webpage site layout
+    consoleNotebook.insertCodeBlock('code-1',0,43);
+
+     //Algorithm logging
+    
+  });
