fbb51520c9dc70b5e3e2f1c8fbab0c35eb455592
Johnny Cowboy <johnnycowboy3033@gmail.com>
2024-04-29 17:51:48 -0400

    commit

---
+++ binary.search.js
@@ -0,0 +1,42 @@
+// Program to implement iterative Binary Search
+ 
+// A iterative binary search function. It returns
+// location of x in given array arr[l..r] is present,
+// otherwise -1
+
+let logging;
+let counter;
+
+function binarySearch(arr, x)
+{  
+
+    logging.push({Action:"ENTER_POINT", Counter:counter++, FunctionName:"binarySearh" });
+
+    let h = 0;
+    let t = arr.length - 1;
+    let mid;
+
+    while (t >= h) {
+         mid = h + Math.floor((t - h) / 2);
+         logging.push({ Action:"VARIABLES", Counter:counter, header:h, midpoint:mid, tail:t });
+  
+        logging.push({Action:"MESSAGE", Counter:counter++, message:`If the element is present at the middle itself`});
+        if (arr[mid] == x){
+            return mid;
+        }
+            
+
+        
+        if (arr[mid] > x){
+            logging.push({Action:"MESSAGE", Counter:counter++, message:`If element is smaller than mid, then it can only be present in left subarray`});
+            t = mid - 1;
+        }else{
+            logging.push({Action:"MESSAGE", Counter:counter++, message:`Else the element can only be present in right subarray`});
+            h = mid + 1;
+        }
+            
+    }
+  
+    logging.push({Action:"MESSAGE", Counter:counter++, message:`We reach here when element is not present in array`});
+    return -1;
+}
--- index.js
+++ index.js
@@ -5,6 +5,20 @@ $(function() {
     //Webpage site layout
     consoleNotebook.insertCodeBlock('code-1',0,43);
 
+    consoleNotebook.clearPage();
+    consoleNotebook.format.manyNewLine(6);
+    consoleNotebook.setClass("bottom-page");
+
      //Algorithm logging
+     function Algorithm(arr,x){
+        let result = binarySearch(arr, x);
+
+        consoleNotebook.insertSteps("steps-algorithm",logging);
+
+    };
+
+     logging = new Array();
+     counter = 0;
+     Algorithm(new Array(0, 10, 20, 30, 40, 50, 60),0);
     
   });
